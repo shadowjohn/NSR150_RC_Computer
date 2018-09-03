1cb8153eb417afc3c7a475060fe81642c7c4349e
羽山 <linainverseshadow@gmail.com>
2018-09-04 01:47:06 +0800

    初次簽入

---
+++ NSR150_RC_COMPUTER/NSR150_RC_COMPUTER.ino
@@ -0,0 +1,244 @@
+/*
+ * NSR-150 RC電腦 V0.1
+ * Release Date: 2018-09-04
+ * * By 羽山秋人
+ * GitHub: https://github.com/shadowjohn/NSR150_RC_Computer
+ * 
+ * 接腳參考
+   #define enA D0
+   #define in1 D1
+   #define in2 D2
+   #define rpm D3
+*/
+
+int analogPin= A0;
+int raw= 0;
+float Vin = 5.0;
+float Vout = 0;
+float R1 = 1000; //R1 是參考電阻
+float R2 = 0; //記錄 RC 目前的電阻值，如 1000Ω~4300Ω
+float buffer= 0;
+
+//long randRPM;
+long now;
+long startMillis;
+long currentMillis;
+
+
+
+bool start_test_flag = true;
+int min_data = 0;
+int max_data = 0;
+
+
+int step_10 = 0;
+
+//用來測試爬梯子的轉速訊號
+//int rpm_arr[7] = { 0,2000,4000,6000,8000,10000,12000 };
+//int rs = 0;
+//int rs_am = 1;
+
+//用來偵測 D3 腳抓引擎轉速的部分
+const int firePin = D3;
+const int fireInt = 0;
+const unsigned int pulsesPR = 1;
+unsigned long lastPT = 0;
+unsigned long rpm = 0;
+int rpm_int;
+int rpm_to_disp;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  randomSeed(analogRead(0)); 
+  pinMode(firePin, INPUT);
+  attachInterrupt(fireInt, &fireIsr, RISING); 
+  
+}
+void fireIsr()
+{
+  unsigned long now = micros();
+  unsigned long duringInt = now - lastPT;
+  if (duringInt > 5000)
+  {
+     rpm = 60000000UL/(duringInt * pulsesPR); //轉速訊號會在這取得!!!
+     lastPT = now;
+     //rpm_int=int(rpm);
+  }  
+  
+}
+void rc_stop()
+{
+  //函式五，馬達停止
+  Serial.println("Run rc_stop");
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);      
+}
+void rc_add(){
+  //函式三：馬達旋轉(RC開)
+  Serial.println("Run rc_add");
+  digitalWrite(D0, HIGH);  
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, HIGH);
+  delay(10);
+}
+void rc_minus(){
+  //函式四：馬達旋轉(RC關)
+  Serial.println("Run rc_minus");
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, LOW);    
+  delay(10);
+}
+void updateR2(){
+  //取得目前RC姿態，將電阻值存到 R2
+  raw= analogRead(analogPin);
+  if(raw) 
+  {
+    buffer= raw * Vin;
+    //Serial.print("Vin:");
+    //Serial.println(Vin);
+    //Serial.print("raw:");
+    //Serial.println(raw);  
+    
+    Vout= (buffer)/1350.0;
+    buffer= (Vin/Vout) -1;
+    R2= R1 * buffer;
+    //Serial.print("Vout: ");
+    //Serial.println(Vout);
+    //Serial.print("R2: ");
+    //Serial.println(R2);
+    //delay(200);
+  }
+}
+int rpm_to_step(int rpm)
+{
+  //函式二：什麼樣的轉速要用第幾級
+  if(rpm<5000)
+  {
+    return 1;
+  }
+  if(rpm>=5000 & rpm < 5500)
+  {
+    return 2;
+  }
+  if(rpm>=5500 & rpm < 6500)
+  {
+    return 3;
+  }
+  if(rpm >= 6500 && rpm < 7000)
+  {
+    return 4;
+  }  
+  if(rpm >= 7000)
+  {
+    return 5;
+  }  
+}
+void rc_run_step(int step)
+{
+  //函式六：依轉速自動旋轉至對應級別
+  int r2_now_step = (R2-min_data) / step_10; 
+  Serial.println();
+  Serial.print("R2 now step :");
+  Serial.println(r2_now_step);
+  Serial.println();
+  
+  if(r2_now_step==step)
+  {
+    rc_stop();
+  }
+  else if(r2_now_step < step)
+  {
+    if(r2_now_step < 5)
+    {
+      rc_add();
+    }
+    else
+    {
+      rc_stop();
+    }
+  }  
+  else if(r2_now_step > step)
+  {
+    if( r2_now_step > 1)
+    {
+      rc_minus();
+    }
+    else
+    {
+      rc_stop();
+    }
+  }  
+}
+void loop()
+{  
+  //依D3取得引擎轉速
+  noInterrupts();
+  rpm_to_disp=int(rpm);
+  interrupts();
+  Serial.println();
+  Serial.print("Now RPM :");
+  Serial.println(rpm_to_disp);
+
+  
+  if(start_test_flag==true) ////測試模式
+  {          
+    //開機時進入測試模式， RC 開關二次，第二次記錄最低點電阻、最高點電阻當參考值
+    rc_add(); 
+    delay(1000);    
+    rc_minus(); 
+    delay(1000);
+    rc_stop();
+    updateR2();
+    min_data=R2;             
+    rc_add(); 
+    delay(1000);
+    rc_stop();
+    updateR2();
+    max_data=R2;
+    rc_minus(); 
+    delay(1000);
+    start_test_flag=false;
+    startMillis = millis();
+    step_10 = (max_data - min_data) / 6;
+
+    //用來測試隨機轉速
+    //randRPM = random(0,11000);
+  }    
+  currentMillis = millis();
+  /*Serial.println();
+  Serial.print("R2:");
+  Serial.println(R2);
+  Serial.print("min_data:");
+  Serial.println(min_data);
+  Serial.print("max_data:");
+  Serial.println(max_data);  
+  */
+  rc_stop();  
+  updateR2();  
+  //delay(100);
+  /*if(currentMillis-startMillis>=1000)
+  {
+    startMillis=currentMillis;
+    rs=rs+rs_am;
+    randRPM = rpm_arr[rs];    
+    if(rs> (sizeof(rpm_arr)/sizeof(int))-1) rs_am *=-1;
+    if(rs<=0) rs_am*=-1;
+    
+  }  
+  */
+  int rpm_steps = rpm_to_step(rpm_to_disp);  
+  Serial.println();
+  //Serial.print("randRPM:");
+  //Serial.print(randRPM);
+  Serial.print(", step:");
+  Serial.print(rpm_steps);
+  rc_run_step(rpm_steps);
+
+}
+
---
+++ README.md
@@ -0,0 +1,42 @@
+
+功能：NSR150 RC 電腦
+
+作者：羽山秋人 (http://3wa.tw)
+
+最初開發日期：2018-08-29 12:05
+最後更新日期：2018-09-04 01:23
+版本：V0.1
+
+說明：
+　　利用 Arduino ESP8266 + DC 壓降控制器 Buck + 馬達控制器 L298N + TTL IC 74LS14 製作的 NSR150 RC 電腦。
+從NSR150維修手冊，可以得知 RC 馬達的測量方法，從 「白/白黑」線餵入「正向、反向」的12V電壓，可讓馬達旋轉。
+然後 RC 伺服馬達可以量測「綠線、紅白」來取得目前RC伺服馬達的電阻，將馬達轉至 Lo ，測量約 1000Ω，轉至 Hi，
+可以量到約 4000Ω。
+
+利用此特性，與 Arduino 腳位 A0 可以拿來作 Ohm Meter (電阻測量器)。
+然後使用 D0，D1，D2 來當作馬達驅動器腳位，連接 L298N：
+D0 -> enA
+D1 -> In1
+D2 -> In2
+
+L298N 輸出的 Out1 -> RC 馬達的「白/黑」
+L298N 輸出的 Out2 -> RC 馬達的「白」
+
+RC 需要 12V 的電壓，將機車的 12V 餵入 L298N 後使用即可。
+Arduino 所需要的 5V 電源，將機車 12V 電源輸入，從 DC 壓降供應BUCK 穩定取得 5V 電源。
+
+D3 接腳用來抓取引擎訊號，引擎訊號透過 74LS14 過濾雜訊。
+
+電路圖：
+  <img src="Circuit_diagram/V0.1.jpg">
+
+詳細心得資料：
+  NSR-RC伺服馬達研究-第一章：http://3wa.tw/blog/blog.php?id=1457
+  NSR-RC伺服馬達研究-第二章：http://3wa.tw/blog/blog.php?id=1687
+  NSR-RC伺服馬達研究-第三章：http://3wa.tw/blog/blog.php?id=1688
+
+參考資料：
+  一、Arduino ohm meter: http://www.circuitbasics.com/arduino-ohm-meter/
+  二、L298N 使用方法: https://howtomechatronics.com/tutorials/arduino/arduino-dc-motor-control-tutorial-l298n-pwm-h-bridge/
+  三、抓取引擎訊號的二種解法: https://kokoraskostas.blogspot.com/2013/12/arduino-inductive-spark-plug-sensor.html
+  四、74LS14 參考資料: http://www.onsemi.com/pub/Collateral/SN74LS14-D.PDF
